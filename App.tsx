98149885ff73b2cd7be34ac2c84752bbd311f9d1
theapache64 <theapache64@gmail.com>
2018-07-24 19:19:10 +0530

    First commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * 
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ * 
+ * @format
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { SplashScreen } from './src/screens/SplashScreen';
+
+const RootStack = createStackNavigator(
+  {
+    SplashScreen: {
+      screen: SplashScreen,
+      navigationOptions: {
+        headerMode: 'none'
+      }
+    }
+  },
+  {
+    initialRouteName: 'SplashScreen'
+  }
+);
+
+interface Props { }
+export class App extends Component<Props> {
+  render() {
+    return (
+      <RootStack />
+    );
+  }
+}
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,20 @@
+import { default as React, PureComponent } from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {
+
+}
+
+interface States {
+
+}
+
+export class SplashScreen extends PureComponent<Props, States> {
+  render() {
+    return (
+      <View>
+        <Text>SplashScreen</Text>
+      </View >
+    );
+  }
+}
