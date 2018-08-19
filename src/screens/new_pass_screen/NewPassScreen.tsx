d557a5469409b504a19df07d81266c02db107170
theapache64 <theapache64@gmail.com>
2018-08-19 18:48:06 +0530

    Connecting new pass screen

--- src/screens/new_pass_screen/NewPassScreen.tsx
+++ src/screens/new_pass_screen/NewPassScreen.tsx
@@ -3,6 +3,11 @@ import { View } from 'react-native';
 import { GuerillaText } from '../../guerilla/widgets/guerialla_text/GuerillaText';
 import { connect } from 'react-redux';
 import { RootReducer } from '../../reducers/RootReducer';
+import { Dispatch } from 'redux';
+import { loadIssuePass } from '../../api/routes/LoadIssuePass';
+import { BaseNetworkShieldScreen } from '../base/BaseNetworkShieldScreen';
+import { LoadIssuePassResponse } from '../../api/responses/LoadIssuePassResponse';
+import { NetworkResponse } from '../../guerilla/utils/api/NetworkResponse';
 
 interface Props {
   title: string;
@@ -12,20 +17,40 @@ interface States {
 
 }
 
-class NewPassScreen extends PureComponent<Props, States> {
-  render(): ReactElement<Props> {
-    return (
-      <View>
-        <GuerillaText>NewPassScreen</GuerillaText>
-      </View >
-    );
+class NewPassScreen
+  extends BaseNetworkShieldScreen<
+  LoadIssuePassResponse,
+  Props,
+  States
+  > {
+
+  renderNetworkShieldScreen(response: LoadIssuePassResponse): ReactElement<any> {
+    throw new Error('Method not implemented.');
   }
+
+  load(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getResponseType(): any {
+    throw new Error('Method not implemented.');
+  }
+
+  getResponse(): NetworkResponse<LoadIssuePassResponse> {
+    throw new Error('Method not implemented.');
+  }
+
 }
 
 const mapStateToProps = (rootReducer: RootReducer) => ({
   guardReducer: rootReducer.guardReducer
 });
 
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  loadIssuePass: (apiKey: string) => dispatch(loadIssuePass(apiKey))
+});
+
 export const newPassScreen = connect(
   mapStateToProps,
+  mapDispatchToProps
 )(NewPassScreen);
