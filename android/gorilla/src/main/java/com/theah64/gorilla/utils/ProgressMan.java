b22c4f4093ee486d2b4adfc65c2f225d93e72698
theapache64 <theapache64@gmail.com>
2018-09-23 21:34:05 +0530

    added nice comments

--- android/gorilla/src/main/java/com/theah64/gorilla/utils/ProgressMan.java
+++ android/gorilla/src/main/java/com/theah64/gorilla/utils/ProgressMan.java
@@ -28,26 +28,28 @@ public class ProgressMan {
         Inject progressLayout
         */
 
-        View rootView = (ViewGroup) ((ViewGroup) activity
-                .findViewById(android.R.id.content)).getChildAt(0);
+        ViewGroup content = activity.findViewById(android.R.id.content);
+        View rootView = content.getChildAt(0);
 
         if (rootView instanceof LinearLayout) {
-            // Removing root view
-            ((ViewGroup) activity.findViewById(android.R.id.content)).removeViewAt(0);
 
+            // Removing root view
+            content.removeView(rootView);
+
+            // Building new frame layout
             final FrameLayout frameLayout = new FrameLayout(activity);
             final int width = FrameLayout.LayoutParams.MATCH_PARENT;
             final int height = FrameLayout.LayoutParams.MATCH_PARENT;
             final FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(width, height);
             frameLayout.setLayoutParams(layoutParams);
+
+            // Adding removed rootView to wrap
             frameLayout.addView(rootView);
 
+            // Setting final layout
             activity.setContentView(frameLayout);
         }
 
-        View rootView2 = (ViewGroup) ((ViewGroup) activity
-                .findViewById(android.R.id.content)).getChildAt(0);
-        Toast.makeText(activity, (rootView2 instanceof FrameLayout) + "", Toast.LENGTH_SHORT).show();
 
     }
 
